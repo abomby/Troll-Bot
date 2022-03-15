10ddd7244a299eca0a55867033cbc89492cf76dc
Anthony Composto <66045966+abomby@users.noreply.github.com>
2022-03-15 16:38:03 -0500

    V1 Working

---
+++ demo/send_message.py
@@ -0,0 +1,10 @@
+import email, base64
+
+def send_message(service, user_id, message):
+  try:
+    message = service.users().messages().send(userId=user_id, body=message).execute()
+    print('Message Id: %s' % message['id'])
+    return message
+  except Exception as e:
+    print('An error occurred: %s' % e)
+    return None
